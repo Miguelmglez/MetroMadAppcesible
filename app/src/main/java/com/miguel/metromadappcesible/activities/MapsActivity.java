45f00a40191677336b2706a10b2f1fa2e82391a7
Miguelmglez <m.marotog@alumnos.urjc.es>
2016-12-21 14:00:47 +0100

    Location enabled

--- app/src/main/java/com/miguel/metromadappcesible/activities/MapsActivity.java
+++ app/src/main/java/com/miguel/metromadappcesible/activities/MapsActivity.java
@@ -1,85 +1,101 @@
 package com.miguel.metromadappcesible.activities;
 
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import org.osmdroid.api.IGeoPoint;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.ScaleBarOverlay;
 import org.osmdroid.views.overlay.gestures.RotationGestureOverlay;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends AppCompatActivity {
+
+    public Location locationGPS;
+    public LocationManager locationManager;
     private MapView myOpenMapView;
     private MapController myMapController;
-    private MyLocationNewOverlay mLocationOverlay;
-    private ScaleBarOverlay mScaleBarOverlay;
-    private RotationGestureOverlay mRotationGestureOverlay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        final Context context = this.getBaseContext();
-        final DisplayMetrics dm = context.getResources().getDisplayMetrics();
-        myOpenMapView = (MapView)findViewById(R.id.map);
+        myOpenMapView = (MapView) findViewById(R.id.map);
         myOpenMapView.setTileSource(TileSourceFactory.MAPNIK);
         myOpenMapView.setUseDataConnection(true);
         myOpenMapView.setMultiTouchControls(true);
-        mRotationGestureOverlay = new RotationGestureOverlay(myOpenMapView);
-        mRotationGestureOverlay.setEnabled(true);
-        this.mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(context), myOpenMapView);
-        mScaleBarOverlay = new ScaleBarOverlay(myOpenMapView);
-        mScaleBarOverlay.setCentred(true);
-        mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
-        mRotationGestureOverlay = new RotationGestureOverlay(myOpenMapView);
-        mRotationGestureOverlay.setEnabled(true);
-        myOpenMapView.getOverlays().add(this.mLocationOverlay);
-        mLocationOverlay.enableMyLocation();
-
-        //this.myLocationOverlay.enableMyLocation();
-        //this.myLocationOverlay.enableFollowLocation();
-
-        //GeoPoint location = new GeoPoint(40.37018451188382,-3.6134711039363103);
-        //double myLocationLat = myLocation.getLatitude();
-        // double myLocationLong = myLocation.getLongitude();
-        //GeoPoint startPoint = new GeoPoint(myLocationLat,myLocationLong);
-        //GeoPoint myPoint = myLocationOverlay.getMyLocation();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        IGeoPoint punto = new GeoPoint(locationGPS.getLatitude(),locationGPS.getLongitude());
         myMapController = (MapController) myOpenMapView.getController();
         myMapController.setZoom(18);
-
-
-        //myMapController.animateTo(location);
-        //myMapController.setCenter(location);
-        //myMapController.setCenter(location);
-
-
-        //important! set your user agent to prevent getting banned from the osm servers
-
-        /*
-        org.osmdroid.tileprovider.constants.OpenStreetMapTileProviderConstants.setUserAgentValue(BuildConfig.APPLICATION_ID);
-        MapView map = (MapView) findViewById(R.id.map);
-        map.setTileSource(TileSourceFactory.PUBLIC_TRANSPORT);
-        map.setBuiltInZoomControls(true);
-        map.setMultiTouchControls(true);
-        double myLocationLat = map.getLatitudeSpanDouble();
-        double myLocationLong = map.getLongitudeSpanDouble();
-        IMapController mapController = map.getController();
-        mapController.setZoom(9);
-        GeoPoint startPoint = new GeoPoint(myLocationLat,myLocationLong);
-        mapController.setCenter(startPoint);
-        */
+        myMapController.setCenter(punto);
+        myMapController.animateTo(punto);
     }
-    public void routes (View v){
+
+    public void routes(View v) {
         Intent intent = new Intent(this, RoutesActivity.class);
         startActivity(intent);
     }
 
+    public void locateMe(View v) {
+        locationGPS = this.damePuntoNuevo(locationGPS);
+        IGeoPoint punto = new GeoPoint(locationGPS.getLatitude(), locationGPS.getLongitude());
+        myMapController = (MapController) myOpenMapView.getController();
+        myMapController.setZoom(18);
+        myMapController.setCenter(punto);
+        myMapController.animateTo(punto);
+    }
+    public Location damePuntoNuevo(Location puntoAntiguo){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return puntoAntiguo;
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return puntoAntiguo;
+        }
 
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+            }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        });
+        locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        return locationGPS;
+    }
 }
--- app/src/main/java/com/miguel/metromadappcesible/activities/SolutionActivity.java
+++ app/src/main/java/com/miguel/metromadappcesible/activities/SolutionActivity.java
@@ -1,11 +1,19 @@
 package com.miguel.metromadappcesible.activities;
 
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import org.osmdroid.api.IGeoPoint;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
@@ -15,22 +23,58 @@ import org.osmdroid.views.MapView;
 public class SolutionActivity extends AppCompatActivity {
     private MapView myOpenMapView;
     private MapController myMapController;
+    public Location locationGPS;
+    public LocationManager locationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solution);
         myOpenMapView = (MapView)findViewById(R.id.mapSolution);
         myOpenMapView.setTileSource(TileSourceFactory.MAPNIK);
+        myOpenMapView.setMultiTouchControls(true);
+        myOpenMapView.setUseDataConnection(true);
+
         GeoPoint location = new GeoPoint(40.37018451188382,-3.6134711039363103);
         myMapController = (MapController) myOpenMapView.getController();
         myMapController.setZoom(18);
-        myOpenMapView.setUseDataConnection(true);
         myMapController.animateTo(location);
-        myOpenMapView.setMultiTouchControls(true);
         myMapController.setCenter(location);
     }
     public void details (View v){
         Intent intent = new Intent(this, DetailsActivity.class);
         startActivity(intent);
+    }
+    public void locateMe(View v) {
+        locationGPS = this.damePuntoNuevo(locationGPS);
+        IGeoPoint punto = new GeoPoint(locationGPS.getLatitude(), locationGPS.getLongitude());
+        myMapController = (MapController) myOpenMapView.getController();
+        myMapController.setZoom(18);
+        myMapController.setCenter(punto);
+        myMapController.animateTo(punto);
+    }
+    public Location damePuntoNuevo(Location puntoAntiguo){
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return puntoAntiguo;
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return puntoAntiguo;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+            }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        });
+        locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        return locationGPS;
     }
 }
